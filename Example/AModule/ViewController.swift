10032871d0ef9252802de07bec537537f375b11a
YueKun <yuekun@tom.com>
2020-03-11 19:25:44 +0800

    Initial commit

---
+++ Example/AModule/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AModule
+//
+//  Created by m.yuekun@outlook.com on 03/11/2020.
+//  Copyright (c) 2020 m.yuekun@outlook.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
